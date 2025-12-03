31cbeb89f076f95e790cb784a5c022aaa802a2cf
HoangTuGio <khangdeptrai198@gmail.com>
2025-12-04 00:25:14 +0700

    [CI][TEST]: Add health check endpoint to TestController for make sure it work

--- src/main/java/hoangtugio/org/inblue/Controller/TestController.java
+++ src/main/java/hoangtugio/org/inblue/Controller/TestController.java
@@ -27,4 +27,9 @@ public class TestController {
     public String ping() {
         return "pong";
     }
+
+    @GetMapping ("/health")
+    public String health() {
+        return "OK";
+    }
 }
