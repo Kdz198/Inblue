85555e144baf8c8cb48f2bba421b1089bba420d5
HoangTuGio <khangdeptrai198@gmail.com>
2025-12-12 23:43:09 +0700

    [TEST][CI]: Enable CORS for UserController to allow cross-origin requests

--- src/main/java/fpt/org/inblue/controller/UserController.java
+++ src/main/java/fpt/org/inblue/controller/UserController.java
@@ -4,15 +4,13 @@ import fpt.org.inblue.model.User;
 import fpt.org.inblue.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin( "*")
 public class UserController {
     @Autowired
     private UserService userService;
