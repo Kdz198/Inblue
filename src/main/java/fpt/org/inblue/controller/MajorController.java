2a10a77c97327870af0ed5f0c54d599636e4a5c2
Thu ha <nha3697@gmail.com>
2026-01-16 14:09:23 +0700

    feat: update MajorController endpoint and enable CORS

--- src/main/java/fpt/org/inblue/controller/MajorController.java
+++ src/main/java/fpt/org/inblue/controller/MajorController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/question-majors")
+@RequestMapping("/api/majors")
+@CrossOrigin(origins = "*")
 public class MajorController {
     @Autowired
     private MajorService questionMajorService;
