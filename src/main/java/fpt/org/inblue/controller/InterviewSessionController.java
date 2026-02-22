0ffb00c43af46bc602a35107e291c5c8353541bb
HoangTuGio <khangdeptrai198@gmail.com>
2026-02-22 23:10:13 +0700

    feat: enable CORS for InterviewSessionController

--- src/main/java/fpt/org/inblue/controller/InterviewSessionController.java
+++ src/main/java/fpt/org/inblue/controller/InterviewSessionController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping ("/api/interview-sessions")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class InterviewSessionController {
 
