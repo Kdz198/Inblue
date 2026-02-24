9d99a07b6c3b96d63fa64d8e335d3b0d66165ad0
Thu ha <nha3697@gmail.com>
2026-02-24 20:41:06 +0700

    feat: enhance PracticeAIResponse DTO with additional fields for practice set details

--- src/main/java/fpt/org/inblue/model/dto/response/PracticeAIResponse.java
+++ src/main/java/fpt/org/inblue/model/dto/response/PracticeAIResponse.java
@@ -1,11 +1,15 @@
 package fpt.org.inblue.model.dto.response;
 
 import fpt.org.inblue.model.dto.request.PracticeQuestionRequest;
+import fpt.org.inblue.model.enums.TargetLevel;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class PracticeAIResponse {
-    List<List<PracticeQuestionRequest>> questions;
+    String practiceSetName;
+    String objective;
+    int dateNumber;
+    List<PracticeQuestionRequest> questions;
 }
