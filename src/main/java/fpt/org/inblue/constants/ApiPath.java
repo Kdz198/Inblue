73d1b289a4521ada0fc80169df407ace13d56cb6
HoangTuGio <khangdeptrai198@gmail.com>
2026-03-02 20:39:47 +0700

    feat: add endpoint for generating quiz items in report API

--- src/main/java/fpt/org/inblue/constants/ApiPath.java
+++ src/main/java/fpt/org/inblue/constants/ApiPath.java
@@ -10,4 +10,5 @@ public class ApiPath {
     public static final String OVERVIEW_FEEDBACK_API = "/api/v1/orchestrator/generate-overview";
     public static final String PROCTORING_SNAPSHOT_API = "/api/v1/proctoring/snapshot";
     public static final String GENERATE_PRACTICE_SET_API = "/api/v1/report/generate-practice";
+    public static final String GENERATE_QUIZ_ITEM_API = "/api/v1/report/generate-quiz";
 }
