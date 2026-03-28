e6ea5e6d09a863b4aacb2744fc1bee996cbd6a47
Thu ha <nha3697@gmail.com>
2026-03-28 15:56:31 +0700

    fix: auth controller

--- src/main/java/fpt/org/inblue/security/SecurityConfig.java
+++ src/main/java/fpt/org/inblue/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration corsConfiguration = new CorsConfiguration();
-                    corsConfiguration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://localhost:8080",frontendUrl));
+                    corsConfiguration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://localhost:8080","https://exe-fe-zeta.vercel.app"));
                     corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
                     corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
                     corsConfiguration.setAllowCredentials(true);
