92ac1845f8db49cad06c191836aa303a5fdf807e
Thu ha <nha3697@gmail.com>
2026-04-09 20:50:05 +0700

    feat: enable CORS for DashboardController to allow cross-origin requests

--- src/main/java/fpt/org/inblue/controller/DashboardController.java
+++ src/main/java/fpt/org/inblue/controller/DashboardController.java
@@ -5,6 +5,7 @@ import fpt.org.inblue.model.Transaction;
 import fpt.org.inblue.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/dashboard")
+@CrossOrigin(origins = "*")
 public class DashboardController {
     @Autowired
     DashboardService dashboardService;
