084a75a87acda5a6fb75124963a888feff158bcd
HoangTuGio <khangdeptrai198@gmail.com>
2026-06-21 15:50:14 +0700

    feat: update workspace generate quiz anythingLLM

--- src/main/java/fpt/org/inblue/enums/AnythingLlmWorkspace.java
+++ src/main/java/fpt/org/inblue/enums/AnythingLlmWorkspace.java
@@ -6,15 +6,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AnythingLlmWorkspace {
-    GENERAL_CHAT("inblue-general-chat"), // Workspace chat thông thường (GPT-4o)
-    DOCS_ANALYSIS("inblue-docs-reader"), // Workspace nhai file lớn (Gemini)
-    HR_SUPPORT("inblue-hr-internal"),    // Workspace dùng RAG tài liệu nội bộ
     CV_ANALYSIS("cv-processor"), // Workspace phân tích CV ứng viên (Gemini)
     CODING_GEN("coding-gen"), // Workspace chuyên về coding (Gemini Pro)
     EMAIL( "email-processor"), // Workspace chuyên về email (Gemini Pro)
     CODE_REVIEW("code-review-processor"), // Workspace chuyên chấm Code Review
-    CODE_REVIEW_GEN("code-review-generator"); // Workspace chuyên sinh đề Code Review
-
+    CODE_REVIEW_GEN("code-review-generator"), // Workspace chuyên sinh đề Code Review
+    QUIZ_GEN("quiz-generator"); // Workspace chuyên sinh đề Quiz
     private final String slug;
 
 
