f4928ab0884be031c415c06cbbaa3349e64f55e8
HoangTuGio <khangdeptrai198@gmail.com>
2026-06-25 17:32:41 +0000

    style: auto format code with spotless [skip ci]

--- src/main/java/fpt/org/inblue/controller/TestSpotlessController.java
+++ src/main/java/fpt/org/inblue/controller/TestSpotlessController.java
@@ -1,10 +1,18 @@
 package fpt.org.inblue.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController public class TestSpotlessController {
-@GetMapping("/api/test-spotless")
-public String test(){
-int a=1;int b=2;
-if(a==1){return "Hello";}else{return "World";}
-}
+
+@RestController
+public class TestSpotlessController {
+    @GetMapping("/api/test-spotless")
+    public String test() {
+        int a = 1;
+        int b = 2;
+        if (a == 1) {
+            return "Hello";
+        } else {
+            return "World";
+        }
+    }
 }
