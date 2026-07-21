0665dafa988e726d9bab3d04a5f1b555ce8bfb3b
HoangTuGio <khangdeptrai198@gmail.com>
2026-07-21 21:51:47 +0700

    refactor: rename timestamp fields in QuestionBank for consistency

--- src/main/java/fpt/org/inblue/model/QuestionBank.java
+++ src/main/java/fpt/org/inblue/model/QuestionBank.java
@@ -2,6 +2,7 @@ package fpt.org.inblue.model;
 
 import fpt.org.inblue.enums.QuestionLevel;
 import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -44,8 +45,8 @@ public class QuestionBank {
     private Boolean isDeleted = false;
 
     @CreationTimestamp
-    private LocalDateTime createdDate;
+    private LocalDateTime  createdAt;
 
     @UpdateTimestamp
-    private LocalDateTime updatedDate;
+    private LocalDateTime  updatedAt;
 }
