2aa83752bf727abfe91d352af7c1271d8ce7049f
HoangTuGio <khangdeptrai198@gmail.com>
2026-07-21 15:22:44 +0000

    style: auto format code with spotless [skip ci]

--- src/main/java/fpt/org/inblue/repository/KioskRepository.java
+++ src/main/java/fpt/org/inblue/repository/KioskRepository.java
@@ -1,8 +1,6 @@
 package fpt.org.inblue.repository;
 
 import fpt.org.inblue.model.Kiosk;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KioskRepository extends JpaRepository<Kiosk, Long> {
-}
+public interface KioskRepository extends JpaRepository<Kiosk, Long> {}
