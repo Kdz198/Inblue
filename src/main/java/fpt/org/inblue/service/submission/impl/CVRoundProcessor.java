b8bd3f29517e93382a526027fe20ef0323e9bedb
Thu ha <nha3697@gmail.com>
2026-08-07 21:55:01 +0700

    feat: add file handling in CVRoundProcessor to process uploaded files

--- src/main/java/fpt/org/inblue/service/submission/impl/CVRoundProcessor.java
+++ src/main/java/fpt/org/inblue/service/submission/impl/CVRoundProcessor.java
@@ -7,9 +7,12 @@ import fpt.org.inblue.event.SubmissionEventHandle;
 import fpt.org.inblue.model.dto.ProcessDto;
 import fpt.org.inblue.service.submission.RoundSubmissionProcessor;
 import fpt.org.inblue.service.submission.SubmissionResult;
+import fpt.org.inblue.utils.FileUtil;
+import java.io.File;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @Component
 @RequiredArgsConstructor
@@ -24,6 +27,17 @@ public class CVRoundProcessor implements RoundSubmissionProcessor {
 
     @Override
     public SubmissionResult process(ProcessDto dto) throws IOException {
+        if (dto.getFile() != null && !dto.getFile().isEmpty()) {
+            String absolutePath = FileUtil.saveFile(dto.getFile());
+            File file = FileUtil.getFileByPath(absolutePath);
+            try {
+                MultipartFile multipartFile = FileUtil.convertFileToMultipart(file);
+                dto.setFile(multipartFile);
+            } finally {
+                file.delete();
+            }
+        }
+
         return SubmissionResult.completed(submissionEventHandle.processCvSubmission(dto));
     }
 }
