d02ddd3214afcad5474f6e9769161d45fa70b116
HoangTuGio <khangdeptrai198@gmail.com>
2026-08-17 01:01:16 +0700

    fix: add JsonAlias annotation for MENTROR_REVIEW in RoundType enum

--- src/main/java/fpt/org/inblue/enums/RoundType.java
+++ src/main/java/fpt/org/inblue/enums/RoundType.java
@@ -1,11 +1,14 @@
 package fpt.org.inblue.enums;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public enum RoundType {
     CV_SCREENING, // Lọc CV (Chấm Match Score, Keyword)
     EMAIL_SIMULATOR, // Giao tiếp chuyên nghiệp (Báo cáo sếp, từ chối task)
     QUIZ, // Trắc nghiệm kiến thức (Core, DB, Framework)
     CODING, // Thuật toán / Logic
     CODE_REVIEW, // Đọc hiểu & Bắt lỗi code người khác
+    @JsonAlias("MENTOR_REVIEW")
     MENTROR_REVIEW, // phỏng vấn với mentor (có phần đánh giá mentor)
     AI_INTERVIEW // Vấn đáp trực tiếp (Tech + Behavioral)
 }
