6cc19b40bdf5fae9c1d4a167e35c5aa4461300ee
HoangTuGio <khangdeptrai198@gmail.com>
2026-09-12 16:32:51 +0000

    style: auto format code with spotless [skip ci]

--- src/main/java/fpt/org/inblue/security/SecurityConfig.java
+++ src/main/java/fpt/org/inblue/security/SecurityConfig.java
@@ -205,10 +205,12 @@ public class SecurityConfig {
                         //                                "/api/sessions/create-for-round",
                         //                                "/api/sessions/make-payment")
                         //                        .hasRole(USER)
-//                        .requestMatchers(HttpMethod.GET, "/api/job-descriptions/recommendations")
-//                        .hasRole(USER)
-//                        .requestMatchers(HttpMethod.PUT, "/api/admin/job-recommendation-threshold")
-//                        .hasRole(ADMIN)
+                        //                        .requestMatchers(HttpMethod.GET,
+                        // "/api/job-descriptions/recommendations")
+                        //                        .hasRole(USER)
+                        //                        .requestMatchers(HttpMethod.PUT,
+                        // "/api/admin/job-recommendation-threshold")
+                        //                        .hasRole(ADMIN)
                         .anyRequest()
                         .authenticated())
                 .oauth2Login(
